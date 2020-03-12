c784139daead0c22486785a42ee36efcbb1b1c10
unknown <Administrador@BUTLI0423.animaedu.intranet>
2020-03-12 17:38:23 -0300

    Versão 1.0

---
+++ Calculadora.java
@@ -0,0 +1,33 @@
+import javax.swing.JOptionPane;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		
+		String operador = JOptionPane.showInputDialog("Escolha a operacao desejada: "
+				+ "\n1 - Somar;"
+				+ "\n0 - Sair;");
+		
+		int operacao;
+		operacao = Integer.parseInt(operador);
+		
+		// 
+		if (operacao == 1) {
+			
+			int n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro numero: "));
+			int n2 = Integer.parseInt( JOptionPane.showInputDialog("Digite o segundo numero: "));
+			
+			int soma;
+			soma = n1 + n2;
+			
+			JOptionPane.showMessageDialog(null, "Resultado: " + soma);
+					    
+			
+		}
+		
+		else {
+			
+			System.exit(0);
+			}
+	}
+}
