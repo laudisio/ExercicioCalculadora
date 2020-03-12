0c07bc38c27774d3b28d5107f33af6c02899fb0a
unknown <Administrador@BUTLI0423.animaedu.intranet>
2020-03-12 17:43:09 -0300

    Versão Final

--- Calculadora.java
+++ Calculadora.java
@@ -7,11 +7,14 @@ public class Calculadora {
 		String operador = JOptionPane.showInputDialog("Escolha a operacao desejada: "
 				+ "\n1 - Somar;"
 				+ "\n2 - Subtração;"
+				+ "\n3 - Mutiplicação;"
+				+ "\n4 - Dividir;"
 				+ "\n0 - Sair;");
 		
 		int operacao;
 		operacao = Integer.parseInt(operador);
-		 
+		
+		// 
 		if (operacao == 1) {
 			
 			int n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro numero: "));
@@ -34,7 +37,26 @@ public class Calculadora {
 			JOptionPane.showMessageDialog(null, "Resultado: " + subtrair);
 			
 		}
+		if (operacao == 3) {
+			int n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro numero: "));
+			int n2 = Integer.parseInt( JOptionPane.showInputDialog("Digite o segundo numero: "));
 		
+			int multiplicar;
+			multiplicar = n1 * n2;
+			
+			JOptionPane.showMessageDialog(null, "Resultado: " + multiplicar);
+			
+		}
+		if (operacao == 4) {
+			int n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro numero: "));
+			int n2 = Integer.parseInt( JOptionPane.showInputDialog("Digite o segundo numero: "));
+		
+			int dividir;
+			dividir = n1/n2;
+			
+			JOptionPane.showMessageDialog(null, "Resultado: " + dividir);
+			
+		}
 		else {
 			
 			System.exit(0);
